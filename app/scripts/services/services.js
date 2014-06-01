6e62c9364012602059408477d9bcfcbe990dc122
SCJ <sallapcjoseph@gmail.com>
2014-06-01 21:38:28 +0530

    Tt

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,11 +1,21 @@
 'use strict';
-angular.module('logInAngApp').factory('$exceptionHandler',function($log){
+angular.module('logInAngApp').factory('$exceptionHandler',function($log,printStackTrace){
 	return function (exception){
-	$log.info("In $exceptionHandler");
-	//var log4js = require('log4js');
-	//var logger = log4js.getLogger();
-	//logger.debug("Some debug messages");
-	$log.info(exception.message);
-    $log.info(exception.stack);
-}
+		$log.info("In $exceptionHandler");
+		//var log4js = require('log4js');
+		//var logger = log4js.getLogger();
+		//logger.debug("Some debug messages");
+		$log.info(exception.message);
+	    $log.info(exception.stack);
+	    printStackTrace.pushStackTrace(exception.stack);
+	};
+});
+
+angular.module('logInAngApp').factory('printStackTrace',function($log){
+	return {
+		pushStackTrace : function (data){
+			$log.info('In pushStackTrace');
+			$log.info(data);
+		}
+	};
 });
